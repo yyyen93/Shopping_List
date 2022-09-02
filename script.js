5ee64df9629e6a2ab4189dfb52899191fc663968
yyyen93 <37173505+yyyen93@users.noreply.github.com>
2022-09-02 16:51:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll('li');
+
+
+
+// Add delete button
+function deleteButton(parent) {
+	var deleteButton = parent.appendChild(document.createElement("button"));
+	deleteButton.classList.add('delBtn');
+	deleteButton.appendChild(document.createTextNode('Delete'));
+	deleteButton.addEventListener('click', function () {
+		this.parentElement.remove();
+	})
+}
+
+// Add class 'done' and delete button to the existing 'li'
+for (var i = 0; i < li.length; i++) {
+	li[i].addEventListener('click', function (event) {
+		event.target.classList.toggle('done');
+	});
+	deleteButton(li[i]);
+}
+
+// Add class 'done' to the other 'li'
+ul.addEventListener('click', function () {
+	event.target.classList.toggle('done');
+})
+
+
+
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	deleteButton(li)
+	ul.appendChild(li);
+	input.value = "";
+}
+
+
+
+
+
+
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+// https://stackoverflow.com/questions/61173115/how-to-add-buttons-next-to-each-list-item-to-delete-the-item-when-clicked-on-its
